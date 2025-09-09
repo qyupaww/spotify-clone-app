795d35879b58b7e0bafd5399edf9c9538c1d3b8a
Faisal Rahman <qyupaww@gmail.com>
2025-09-09 14:11:26 +0700

    Feat: Constants Playlist

---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+class SpotifyPlaylistConstants {
+  static const playlistAsset = [
+    "cincin.jpeg",
+    "ego.jpg",
+    "nina.jpeg",
+    "pretender.jpeg",
+    "rise.jpeg",
+  ];
+
+  static const playlistTitle = [
+    "Cincin",
+    "Everything Goes On",
+    "Nina",
+    "Pretender",
+    "RISE",
+  ];
+
+  static const playlistSinger = {
+    "Hindia",
+    "Porter Robbinson",
+    "Feast .",
+    "Official HIGE DANdism",
+    "League of Legends",
+  };
+}
